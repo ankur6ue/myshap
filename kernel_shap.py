b16959d5d977e7b2b9e98c52f95dce5e611eb502
ankur6ue <ankur6ue@gmail.com>
2020-07-29 09:43:55 -0400

    Optimized implementation by eliminating loop over number of coalitions and using batch indices and added batched parallel execution for multiple data instances

--- kernel_shap.py
+++ kernel_shap.py
@@ -1,7 +1,8 @@
 import math
 import numpy as np
 from more_itertools import distinct_permutations
-
+import concurrent.futures
+import os
 
 class KernelShapModel:
 
@@ -10,31 +11,57 @@ class KernelShapModel:
         self.coalitions = None
 
     def run(self, data, _instance, coalition_depth=1):
+        # assert len(X.shape) == 1 or len(X.shape) == 2, "Instance must have 1 or 2 dimensions!"
+        # data = X.reshape((1, X.shape[0]))
         self.num_features = data.shape[1]
-        self.coalitions = self._generate_coalitions(self.num_features, coalition_depth)
+        coalitions = self._generate_coalitions(self.num_features, coalition_depth)
         # num_coalitions = len(self.coalitions)
-        self.instance = _instance
-        model_out = self.predictor(_instance)
-        fx = model_out[0]
-        Y = self._f_hxz(data)
-        pi = self._generate_pi()
-        W = np.diag(pi)
-        X = np.array(self.coalitions)
-        Xn = X[:, -1]  # last column
+        if len(_instance.shape) == 1:
+            _instance = _instance.reshape(1,-1)
+
+        fx = self.predictor(_instance)
         Ef = np.mean(self.predictor(data))
-        Y = Y - Ef - (fx - Ef)*Xn
-        X = X - np.repeat(np.expand_dims(Xn, axis=1), self.num_features, axis=1)
-        XX = X[:, 0:self.num_features-1]
-        left = np.linalg.inv(np.dot(np.dot(XX.transpose(), W), XX))
-        right = np.dot(np.dot(XX.transpose(), W), Y)
-        shap_vals_ = np.dot(left, right)
-        phi0 = Ef
-        phi_n = fx - phi0 - np.sum(shap_vals_)
+        pi = self._generate_pi(coalitions)
+        futures = []
         shap_vals = []
-        shap_vals.append(phi0)
-        for val in shap_vals_:
-            shap_vals.append(val)
-        shap_vals.append(phi_n)
+
+        num_cores = min(5, os.cpu_count())
+        instance_chunks = np.array_split(_instance, num_cores, axis=0)
+        fx_chunks = np.array_split(fx, num_cores, axis=0)
+        num_splits = len(instance_chunks)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for i in range(0, num_splits):
+                futures.append(executor.submit(lambda p: self._shap(*p),
+                                [Ef, fx_chunks[i], data, coalitions, pi, instance_chunks[i]]))
+
+        for future in futures:
+            for s in future.result():
+                shap_vals.append(s)
+        return shap_vals
+
+    def _shap(self, Ef, fx, data, coalitions, pi, _instance):
+        num_instances = _instance.shape[0]
+        shap_vals = []
+        for i in range(0, num_instances):
+            Y = self._f_hxz(data, coalitions, _instance[i])
+            F = data.shape[1]
+            W = np.diag(pi)
+            M = np.array(coalitions)
+            Mn = M[:, -1]  # last column
+            Y = Y - Ef - (fx[i] - Ef) * Mn
+            M = M - np.repeat(np.expand_dims(Mn, axis=1), F, axis=1)
+            MM = M[:, 0: F-1]
+            left = np.linalg.inv(np.dot(np.dot(MM.transpose(), W), MM))
+            right = np.dot(np.dot(MM.transpose(), W), Y)
+            shap_vals_ = np.dot(left, right)
+            phi0 = Ef
+            phi_n = fx - phi0 - np.sum(shap_vals_)
+            shap_vals.append([])
+            shap_vals[i].append(phi0)
+            for val in shap_vals_:
+                shap_vals[i].append(val)
+            shap_vals[i].append(phi_n)
+
         return np.array(shap_vals)
 
     def _coalition_weight(self, num_features, z):
@@ -54,26 +81,26 @@ class KernelShapModel:
 
         return coalition
 
-    def _f_hxz(self, data):
+    def _f_hxz(self, data, coalitions, instance):
         N = data.shape[0]
         # M: number of coalitions
-        M = len(self.coalitions)
-        points = np.zeros((N*M, self.num_features), np.float)
+        M = len(coalitions)
+        # Repeat data M times
+        points = np.tile(data, (M, 1))
         for i in range(0, M):
-            mask = self.coalitions[i]
-            for j in range(0, N):
-                points[j + i*N] = data[j]
-                nz_i = np.nonzero(mask)
-                points[j + i*N, nz_i] = self.instance[0][nz_i]
+            mask = coalitions[i]
+            nz_i = np.nonzero(mask)
+            points[i*N: (i+1)*N, nz_i] = instance[nz_i]
+
         evals = self.predictor(points)
         eY = []
-        for i in range(0, N*M, N):
-            eY.append(np.mean(evals[i: i+N]))
+        for i in range(0, N * M, N):
+            eY.append(np.mean(evals[i: i + N]))
         return np.array(eY)
 
-    def _generate_pi(self):
+    def _generate_pi(self, coalitions):
         weights = []
-        for coal in self.coalitions:
+        for coal in coalitions:
             z = np.count_nonzero(coal)
             weights.append(self._coalition_weight(self.num_features, z))
         return weights
--- kernel_shap_test.py
+++ kernel_shap_test.py
@@ -5,6 +5,7 @@ from more_itertools import distinct_permutations
 from scipy.optimize import minimize
 import shap
 from kernel_shap import KernelShapModel
+import time
 
 # shap_values1 = KernelShapModel(neural_net.predict_proba).run(x[:50], np.array(x_explain).reshape(1,2), 1)
 
@@ -14,8 +15,8 @@ from sklearn import preprocessing
 from sklearn.ensemble import RandomForestRegressor
 # The target variable is 'quality'.
 Y = df['quality']
-# X =  df[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar','chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density','pH', 'sulphates', 'alcohol']]
-X =  df[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar','chlorides']]
+X =  df[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar','chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density','pH', 'sulphates', 'alcohol']]
+#X =  df[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar','chlorides']]
 # Split the data into train and test data:
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
 # Build the model with the random forest regression algorithm:
@@ -23,12 +24,29 @@ model = RandomForestRegressor(max_depth=6, random_state=0, n_estimators=10)
 model.fit(X_train, Y_train)
 
 explainer = shap.KernelExplainer(model.predict, X_train)
-shap_values2 = explainer.shap_values(X_train.iloc[0])
+start = time.time()
+shap_values_1 = explainer.shap_values(X_train.iloc[0:5])
+print("python shap execution time: {0}".format(time.time() - start))
+instance = X_train.iloc[0:5].values
 
-instance = X_train.iloc[0].values
-shap_values = KernelShapModel(model.predict).run(X_train.values, instance.reshape(1, -1), 4)
-print(shap_values)
-## verify shap_vales == shap_values2
+start = time.time()
+coalition_depth = len(X.columns)-1
+shap_values_2 = KernelShapModel(model.predict).run(X_train.values, instance, coalition_depth)
+print("python shap execution time: {0}".format(time.time() - start))
+print(shap_values_2)
+
+shap_values_2 = np.array(shap_values_2)[:, 1:]
+# don't include phi0, because that's not part of the shap_values returned by shap.KernelExplainer
+M, N = shap_values_2.shape
+max_diff = 0
+for i in range(0, M):
+    for j in range(0, N):
+        max_diff = max(shap_values_1[i, j] - shap_values_2[i, j], max_diff)
+
+print(max_diff)
+
+## verify shap_values_1 == shap_values_2
+
 
 
 
