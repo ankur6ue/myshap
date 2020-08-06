d83ddd2d97a78ed5a6ce3e2342a644347369bea8
ankur6ue <ankur6ue@gmail.com>
2020-08-05 22:33:10 -0400

    Added data summarization using Kmeans and added a weights parameter to the run function

--- kernel_shap.py
+++ kernel_shap.py
@@ -1,49 +1,89 @@
 import math
 import numpy as np
 from more_itertools import distinct_permutations
+from sklearn.cluster import KMeans
 import concurrent.futures
 import os
+import types
+
 
 class KernelShapModel:
 
     def __init__(self, predictor):
+        assert callable(predictor), \
+            "predictor must be a callable"
         self.predictor = predictor
         self.coalitions = None
 
-    def run(self, data, _instance, coalition_depth=1):
-        # assert len(X.shape) == 1 or len(X.shape) == 2, "Instance must have 1 or 2 dimensions!"
-        # data = X.reshape((1, X.shape[0]))
-        self.num_features = data.shape[1]
-        coalitions = self._generate_coalitions(self.num_features, coalition_depth)
-        # num_coalitions = len(self.coalitions)
-        if len(_instance.shape) == 1:
-            _instance = _instance.reshape(1,-1)
+    def run(self, data: np.ndarray, weights: np.ndarray, new_data: np.ndarray, coalition_depth:int=1, max_cpus:int=4) \
+            -> np.ndarray:
+        """ Generates KernelSHAP values for data points in data using the Kernel Shap algorithm
 
-        fx = self.predictor(_instance)
-        Ef = np.mean(self.predictor(data))
-        pi = self._generate_pi(coalitions)
-        futures = []
+                Parameters
+                ----------
+                data : numpy.array
+                    Matrix of training data samples (# samples x # features). Can be original data
+                    or summarized data (output of running kmeans on original data)
+
+                weights : numpy.array
+                    weights for each data point. Typically returned by running kmeans on the original data.
+                    If original data is being passed, use 1/(num of data points) as the weights
+
+                 new_data: numpy.array
+                    new data for which shapley values should be computed (# samples x # features)
+                    
+                 coalition_depth : int
+                    coalition depth. This parameter in-turn controls how many coalitions are considered
+                    during shapley value computation. Eg., if coalition_depth = 2, and number of features is 
+                    4, then number of coalitions considered = C(4,1) + C(4,2) = 10. Note that no features present
+                    corresponds to phi0 and all features present correspond to phi_n
+                    
+                 max_cpus : int  
+                    maximum number of cpus to use during parallel computation of shapley values for multiple data
+                    points 
+
+
+                Returns
+                -------
+                Matrix of shapley values for new_data points [new_data samples x num_features]
+                """
+        if (isinstance(data, np.ndarray) and isinstance(weights, np.ndarray) and
+            isinstance(new_data, np.ndarray)):
+            # assert len(X.shape) == 1 or len(X.shape) == 2, "Instance must have 1 or 2 dimensions!"
+            # data = X.reshape((1, X.shape[0]))
+            self.num_features = data.shape[1]
+            coalitions = self._generate_coalitions(self.num_features, coalition_depth)
+            # num_coalitions = len(self.coalitions)
+            if len(new_data.shape) == 1:
+                new_data = new_data.reshape(1,-1)
+
+            fx = self.predictor(new_data)
+            Ef = np.average(self.predictor(data), weights=weights)
+            pi = self._generate_pi(coalitions)
+            futures = []
+            shap_vals = []
+
+            num_cores = min(max_cpus, os.cpu_count())
+            instance_chunks = np.array_split(new_data, num_cores, axis=0)
+            fx_chunks = np.array_split(fx, num_cores, axis=0)
+            num_splits = len(instance_chunks)
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                for i in range(0, num_splits):
+                    futures.append(executor.submit(lambda p: self._shap(*p),
+                                    [Ef, fx_chunks[i], data, weights, coalitions, pi, instance_chunks[i]]))
+
+            for future in futures:
+                for s in future.result():
+                    shap_vals.append(s)
+            return shap_vals
+        else:
+            raise ValueError('input variables must be np.ndarray')
+
+    def _shap(self, Ef, fx, data, weights, coalitions, pi, new_data):
+        numnew_datas = new_data.shape[0]
         shap_vals = []
-
-        num_cores = min(5, os.cpu_count())
-        instance_chunks = np.array_split(_instance, num_cores, axis=0)
-        fx_chunks = np.array_split(fx, num_cores, axis=0)
-        num_splits = len(instance_chunks)
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            for i in range(0, num_splits):
-                futures.append(executor.submit(lambda p: self._shap(*p),
-                                [Ef, fx_chunks[i], data, coalitions, pi, instance_chunks[i]]))
-
-        for future in futures:
-            for s in future.result():
-                shap_vals.append(s)
-        return shap_vals
-
-    def _shap(self, Ef, fx, data, coalitions, pi, _instance):
-        num_instances = _instance.shape[0]
-        shap_vals = []
-        for i in range(0, num_instances):
-            Y = self._f_hxz(data, coalitions, _instance[i])
+        for i in range(0, numnew_datas):
+            Y = self._f_hxz(data, weights, coalitions, new_data[i])
             F = data.shape[1]
             W = np.diag(pi)
             M = np.array(coalitions)
@@ -81,7 +121,7 @@ class KernelShapModel:
 
         return coalition
 
-    def _f_hxz(self, data, coalitions, instance):
+    def _f_hxz(self, data, weights, coalitions, instance):
         N = data.shape[0]
         # M: number of coalitions
         M = len(coalitions)
@@ -95,7 +135,7 @@ class KernelShapModel:
         evals = self.predictor(points)
         eY = []
         for i in range(0, N * M, N):
-            eY.append(np.mean(evals[i: i + N]))
+            eY.append(np.average(evals[i: i + N], weights=weights))
         return np.array(eY)
 
     def _generate_pi(self, coalitions):
@@ -105,6 +145,38 @@ class KernelShapModel:
             weights.append(self._coalition_weight(self.num_features, z))
         return weights
 
+    def kmeans(self, X, k, round_values=True):
+        """ Summarize a dataset with k mean samples weighted by the number of data points they
+        each represent.
 
+        Parameters
+        ----------
+        X : numpy.array
+            Matrix of data samples to summarize (# samples x # features)
+
+        k : int
+            Number of means to use for approximation.
+
+        round_values : bool
+            For all i, round the ith dimension of each mean sample to match the nearest value
+            from X[:,i]. This ensures discrete features always get a valid value.
+
+        Returns
+        -------
+        Cluster centers, cluster weights.
+        """
+
+        kmeans = KMeans(n_clusters=k, random_state=0).fit(X)
+
+        if round_values:
+            for i in range(k):  # for each cluster
+                for j in range(X.shape[1]):  # find closest data point whose feature j is closest to cluster i
+                    ind = np.argmin(np.abs(X[:, j] - kmeans.cluster_centers_[i, j]))
+                    kmeans.cluster_centers_[i, j] = X[ind, j]  # set the j'th feature of cluster i to the closest
+                    # matching data point
+        # The weights of a cluster are proportional to the number of elements in the cluster
+        weights = 1.0 * np.bincount(kmeans.labels_)
+        weights /= np.sum(weights)
+        return kmeans.cluster_centers_, weights
 
 
--- kernel_shap_test.py
+++ kernel_shap_test.py
@@ -1,18 +1,17 @@
 import math
 import pandas as pd
 import numpy as np
-from more_itertools import distinct_permutations
-from scipy.optimize import minimize
 import shap
 from kernel_shap import KernelShapModel
 import time
-
-# shap_values1 = KernelShapModel(neural_net.predict_proba).run(x[:50], np.array(x_explain).reshape(1,2), 1)
-
-df = pd.read_csv('winequality-red.csv') # Load the data
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+shap.initjs()
+# Testing a RandomForest model
+df = pd.read_csv('winequality-red.csv') # Load the data
+
 # The target variable is 'quality'.
 Y = df['quality']
 X =  df[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar','chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density','pH', 'sulphates', 'alcohol']]
@@ -25,18 +24,27 @@ model.fit(X_train, Y_train)
 
 explainer = shap.KernelExplainer(model.predict, X_train)
 start = time.time()
-shap_values_1 = explainer.shap_values(X_train.iloc[0:5])
+shap_values_1 = explainer.shap_values(X_test.iloc[0])
+# shap.summary_plot(shap_values_1, X_test.iloc[0:5])
+shap.decision_plot(explainer.expected_value, shap_values_1, X_test.iloc[0])
+p = shap.force_plot(explainer.expected_value, shap_values_1, X_test.iloc[0])
+
 print("python shap execution time: {0}".format(time.time() - start))
 instance = X_train.iloc[0:5].values
 
 start = time.time()
 coalition_depth = len(X.columns)-1
-shap_values_2 = KernelShapModel(model.predict).run(X_train.values, instance, coalition_depth)
+num_train_samples = X_train.values.shape[0]
+weights = np.ones(num_train_samples)/num_train_samples
+shap_values_2 = KernelShapModel(model.predict).run(X_train.values, weights, instance, coalition_depth)
+
+f = plt.figure()
+f.savefig("/summary_plot1.png", bbox_inches='tight', dpi=600)
 print("python shap execution time: {0}".format(time.time() - start))
 print(shap_values_2)
-
-shap_values_2 = np.array(shap_values_2)[:, 1:]
 # don't include phi0, because that's not part of the shap_values returned by shap.KernelExplainer
+shap_values_2 = np.array(shap_values_2)[:, 1:]
+
 M, N = shap_values_2.shape
 max_diff = 0
 for i in range(0, M):
